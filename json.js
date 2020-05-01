6a9b1ed9bd06d6e3c6f1c5570357c50d759c3b58
Cosmin Mihalache <56051151+c-obsesie@users.noreply.github.com>
2020-05-01 19:17:12 +0100

    Create json.js

---
+++ json.js
@@ -0,0 +1,169 @@
+const mocks = [
+  {
+    id: 1,
+    saved: true,
+    location: '25min',
+    title: 'Basic meditation',
+    preview:
+      'https://images.unsplash.com/photo-1507501336603-6e31db2be093?auto=format&fit=crop&w=800&q=80',
+    adress: 'sal',
+    numMed: '10',
+    meditations: [
+      {
+        id: '1',
+        meditation: '1',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+      {
+        id: '2',
+        meditation: '2',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+      {
+        id: '3',
+        meditation: '3',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+      {
+        id: '4',
+        meditation: '3',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+      {
+        id: '5',
+        meditation: '5',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+    ],
+  },
+  {
+    id: 2,
+    saved: false,
+    location: '20min',
+    title: 'Sleep Meditation',
+    preview:
+      'https://images.unsplash.com/photo-1458906931852-47d88574a008?auto=format&fit=crop&w=800&q=80',
+    adress: '',
+    numMed: '10',
+    meditations: [
+      {
+        id: '1',
+        meditation: '1',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+      {
+        id: '2',
+        meditation: '2',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+      {
+        id: '3',
+        meditation: '3',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+      {
+        id: '4',
+        meditation: '3',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+      {
+        id: '5',
+        meditation: '5',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+    ],
+  },
+  {
+    id: 3,
+    saved: true,
+    location: '20min',
+    title: 'Stress Meditation',
+    preview:
+      'https://images.unsplash.com/photo-1507501336603-6e31db2be093?auto=format&fit=crop&w=800&q=80',
+    adress: '',
+    numMed: '10',
+    meditations: [
+      {
+        id: '1',
+        meditation: '1',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+      {
+        id: '2',
+        meditation: '2',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+      {
+        id: '3',
+        meditation: '3',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+      {
+        id: '4',
+        meditation: '3',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+      {
+        id: '5',
+        meditation: '5',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+    ],
+  },
+  {
+    id: 4,
+    location: '10min',
+    title: 'WorkOut Meditation',
+    preview:
+      'https://images.unsplash.com/photo-1458906931852-47d88574a008?auto=format&fit=crop&w=800&q=80',
+    adress: '',
+    numMed: '10',
+    meditations: [
+      {
+        id: '1',
+        meditation: '1',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+      {
+        id: '2',
+        meditation: '2',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+      {
+        id: '3',
+        meditation: '3',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+      {
+        id: '4',
+        meditation: '3',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+      {
+        id: '5',
+        meditation: '5',
+        meditationPlayerAdress: '',
+        meditationAudioadress: '',
+      },
+    ],
+  },
+];
